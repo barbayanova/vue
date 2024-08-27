986bd000e70f4b931c56c7f3e07f0fc981b030d2
Dasha <barbayanovaaa@yandex.ru>
2024-08-27 20:58:40 +0300

    .

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,3 +1,21 @@
 <template>
-
+    <h1>
+    Проект на Vue
+   <button @click="increment">Счетчик {{ counter2}} </button>
+    </h1>
 </template>
+<script setup>
+import { onMounted, ref } from 'vue'
+const counter = ref(0)
+
+function increment () {
+    counter.value++
+}
+onMounted(() => {
+    console.log(counter);
+    
+})
+
+console.log(counter);
+console.log(counter2);
+</script>
